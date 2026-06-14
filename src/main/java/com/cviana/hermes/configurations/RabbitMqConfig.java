399af0e81774062de727f6124f0b89249ac8e3bc
Carlos Viana <carlosedsviana@gmail.com>
2026-06-14 13:03:28 -0300

    chore: Remoção de dependência sem uso

--- src/main/java/com/cviana/hermes/configurations/RabbitMqConfig.java
+++ src/main/java/com/cviana/hermes/configurations/RabbitMqConfig.java
@@ -6,7 +6,6 @@ import java.util.Map;
 import org.springframework.amqp.core.Binding;
 import org.springframework.amqp.core.BindingBuilder;
 import org.springframework.amqp.core.DirectExchange;
-import org.springframework.amqp.core.ExchangeBuilder;
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.core.QueueBuilder;
 import org.springframework.amqp.core.TopicExchange;
